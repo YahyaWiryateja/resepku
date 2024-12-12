39a4bac86a7f0f620ab8af7d3ee514e377554b3a
YahyaWiryateja <11211085@student.itk.ac.id>
2024-12-13 03:45:02 +0800

    tambah port

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ const SECRET_KEY = "YOUR_VERY_SECURE_SECRET_KEY_REPLACE_IN_PRODUCTION";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
