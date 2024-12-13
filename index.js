1b9ff6b8840f57df978bdef916c68192aad298e8
YahyaWiryateja <11211085@student.itk.ac.id>
2024-12-13 13:46:51 +0800

    railway w

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
 
 
 const SECRET_KEY = "YOUR_VERY_SECURE_SECRET_KEY_REPLACE_IN_PRODUCTION";
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', 
-  password: '', 
-  database: 'resepku'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
