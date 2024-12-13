5fa0b1b9255d6053c03be0933ae1581e9aa9b806
YahyaWiryateja <11211085@student.itk.ac.id>
2024-12-13 23:39:45 +0800

    testing 2

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  ssl: false,
 });
 
 db.connect((err) => {
