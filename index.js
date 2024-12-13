630c4f96a96c4c729b9d2ad2324d128d3034146e
YahyaWiryateja <11211085@student.itk.ac.id>
2024-12-13 14:49:09 +0800

    baikin sql

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 const mysql = require("mysql2");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 
 const SECRET_KEY = "YOUR_VERY_SECURE_SECRET_KEY_REPLACE_IN_PRODUCTION";
@@ -19,7 +19,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
@@ -664,7 +665,7 @@ module.exports = app;
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
 // Global error handler
